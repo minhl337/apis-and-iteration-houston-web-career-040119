e7ed265239ae4a151fefcb6d5b472a9004d74f25
minhl337 <imnotminh@gmail.com>
2019-04-02 16:04:57 -0500

    works

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,6 +2,26 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+response_string = RestClient.get('http://www.swapi.co/api/people/')
+response_hash = JSON.parse(response_string)
+
+# lol=[]
+# swap={}
+
+# response_hash.each do |x,y|
+#   if x == "results"
+#     y.each do |a,b|
+#       lol.push(a["name"])
+#       swap[a["name"]]=a["films"]
+#     end
+#   end
+# end
+
+# binding.pry
+
+
+
+
 def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
@@ -16,11 +36,41 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
+  
+  swap={}
+  
+  response_hash.each do |x,y|
+    if x == "results"
+      y.each do |a,b|
+        if a["name"]=character_name
+          swap[a["name"]]=a["films"]
+        end
+      end
+    end
+  end
+
+  swap
 end
+
+
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+
+  films.each do |x,y|
+    counter=1
+    y.each do |a|
+      url=a
+      aa=RestClient.get(url)
+      bb=JSON.parse(aa)
+      movie_name=bb["title"]
+      puts "#{counter} #{movie_name}"
+      counter +=1
+    end
+  end
 end
+
+
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,12 @@
 def welcome
   # puts out a welcome message here!
+  puts "Hello, young Padiwan."
 end
 
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  user_character=gets.chomp
+  puts "You picked #{user_character}."
+  user_character
 end
